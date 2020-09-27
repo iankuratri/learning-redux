fc67b2e3555788885f5cbb7b550db0ea5a3c5d7f
Ankur Atri <ankuratri.in@gmail.com>
2020-09-27 17:33:28 +0530

    hof

---
+++ 02 Functional Programming in JavaScript/higher-order-functions.js
@@ -0,0 +1,10 @@
+/**
+ *  Higher order function is a function that takes function as an argument or returns it or both.
+ */
+
+let numbers = [2, 4, 5, 6];
+numbers.map((number) => number * 2);
+
+setTimeout(() => {
+  console.log("Hello");
+}, 1000);
