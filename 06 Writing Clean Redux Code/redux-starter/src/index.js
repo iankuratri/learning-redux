52c56a1dc11578dabcf098909de64580b634c3c3
Ankur Atri <ankuratri.in@gmail.com>
2020-12-05 00:15:13 +0530

    exercise: action and reducer for projects added using createSlice fn from reduxjs toolkit

--- 06 Writing Clean Redux Code/redux-starter/src/index.js
+++ 06 Writing Clean Redux Code/redux-starter/src/index.js
@@ -1,5 +1,6 @@
 import configureStore from "./store/configureStore";
-import * as actions from "./store/bugs";
+// import * as actions from "./store/bugs";
+import * as actions from "./store/projects";
 
 const store = configureStore();
 
@@ -8,15 +9,26 @@ const unsubscribe = store.subscribe(() => {
   console.log("Store Changed: ", store.getState());
 });
 
-// Adding a bug
-store.dispatch(actions.bugAdded({ description: "UI Bug 1." }));
-store.dispatch(actions.bugAdded({ description: "UI Bug 2." }));
-store.dispatch(actions.bugAdded({ description: "UI Bug 3." }));
+/**
+ 
+ // Adding bug
+ store.dispatch(actions.bugAdded({ description: "UI Bug 1." }));
+ store.dispatch(actions.bugAdded({ description: "UI Bug 2." }));
+ store.dispatch(actions.bugAdded({ description: "UI Bug 3." }));
+ 
+ // Resolving a bug
+ store.dispatch(actions.bugResolved({ id: 1 }));
+ 
+ // Deleting a bug
+ store.dispatch(actions.bugRemoved({ id: 1 }));
 
-// Resolving a bug
-store.dispatch(actions.bugResolved({ id: 1 }));
+ */
 
-// Deleting a bug
-store.dispatch(actions.bugRemoved({ id: 1 }));
+// Adding project
+store.dispatch(actions.projectAdded({ name: "My Project 1" }));
+store.dispatch(actions.projectAdded({ name: "My Project 2" }));
+
+// Deleting project
+store.dispatch(actions.projectRemoved({ id: 1 }));
 
 unsubscribe();
