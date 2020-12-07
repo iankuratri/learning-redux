901515677cb71f991efd9e6946feed0e977dc4c7
Ankur Atri <ankuratri.in@gmail.com>
2020-12-08 00:00:43 +0530

    projects and bugs reducers combined using combineReducers from reduxjs/toolkit

--- 07 Designing the Store/redux-starter/src/index.js
+++ 07 Designing the Store/redux-starter/src/index.js
@@ -1,6 +1,6 @@
 import configureStore from "./store/configureStore";
-// import * as actions from "./store/bugs";
-import * as actions from "./store/projects";
+import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import { projectAdded, projectRemoved } from "./store/projects";
 
 const store = configureStore();
 
@@ -9,26 +9,22 @@ const unsubscribe = store.subscribe(() => {
   console.log("Store Changed: ", store.getState());
 });
 
-/**
- 
- // Adding bug
- store.dispatch(actions.bugAdded({ description: "UI Bug 1." }));
- store.dispatch(actions.bugAdded({ description: "UI Bug 2." }));
- store.dispatch(actions.bugAdded({ description: "UI Bug 3." }));
- 
- // Resolving a bug
- store.dispatch(actions.bugResolved({ id: 1 }));
- 
- // Deleting a bug
- store.dispatch(actions.bugRemoved({ id: 1 }));
+// Adding bug
+store.dispatch(bugAdded({ description: "UI Bug 1." }));
+store.dispatch(bugAdded({ description: "UI Bug 2." }));
+store.dispatch(bugAdded({ description: "UI Bug 3." }));
 
- */
+// Resolving a bug
+store.dispatch(bugResolved({ id: 1 }));
+
+// Deleting a bug
+store.dispatch(bugRemoved({ id: 1 }));
 
 // Adding project
-store.dispatch(actions.projectAdded({ name: "My Project 1" }));
-store.dispatch(actions.projectAdded({ name: "My Project 2" }));
+store.dispatch(projectAdded({ name: "My Project 1" }));
+store.dispatch(projectAdded({ name: "My Project 2" }));
 
 // Deleting project
-store.dispatch(actions.projectRemoved({ id: 1 }));
+store.dispatch(projectRemoved({ id: 1 }));
 
 unsubscribe();
