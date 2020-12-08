76b9d5441ff5d22f291ac6db18d69a53a15b2259
Ankur Atri <ankuratri.in@gmail.com>
2020-12-08 23:30:34 +0530

    redux thunk used instead of func, thanks to getDefaultMiddleware fn

--- 08 Middleware/src/store/configureStore.js
+++ 08 Middleware/src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
 import func from "./middleware/func";
@@ -6,6 +6,6 @@ import func from "./middleware/func";
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger({ destination: "console" }), func],
+    middleware: [...getDefaultMiddleware(), logger({ destination: "console" })],
   });
 }
