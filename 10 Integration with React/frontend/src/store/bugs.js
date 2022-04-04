28e45b9b382fd51aadb1edbc5373dc06cdd8381b
Ankur Atri <ankur@zenways.io>
2022-04-04 14:33:21 +0530

    store files from old project added

---
+++ 10 Integration with React/frontend/src/store/bugs.js
@@ -0,0 +1,116 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { apiCallBegan } from "./api";
+import moment from "moment";
+
+// Actions and Reducer created using single function createSlice
+const slice = createSlice({
+  name: "bugs",
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null,
+  },
+  reducers: {
+    bugsRequested: (bugs, action) => {
+      bugs.loading = true;
+    },
+
+    bugsRequestFailed: (bugs, action) => {
+      bugs.loading = false;
+    },
+
+    bugsReceived: (bugs, action) => {
+      bugs.list = action.payload;
+      bugs.lastFetch = Date.now();
+      bugs.loading = false;
+    },
+
+    bugAdded: (bugs, action) => {
+      bugs.list.push(action.payload);
+    },
+
+    bugRemoved: (bugs, action) => {
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
+      bugs.list.splice(index, 1);
+    },
+
+    bugResolved: (bugs, action) => {
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
+      bugs.list[index].resolved = true;
+    },
+
+    bugAssignedToUser: (bugs, action) => {
+      const { id: bugId, userId } = action.payload;
+      const index = bugs.list.findIndex((bug) => bug.id === bugId);
+      bugs.list[index].userId = userId;
+    },
+  },
+});
+
+const {
+  bugAdded,
+  bugRemoved,
+  bugResolved,
+  bugAssignedToUser,
+  bugsReceived,
+  bugsRequested,
+  bugsRequestFailed,
+} = slice.actions;
+export default slice.reducer;
+
+// Action creators
+const url = "/bugs";
+
+export const loadBugs = () => (dispatch, getState) => {
+  const { lastFetch } = getState().entities.bugs;
+
+  const diffInMinutes = moment().diff(moment(lastFetch), "minutes");
+  if (diffInMinutes < 10) return;
+
+  dispatch(
+    apiCallBegan({
+      url,
+      onStart: bugsRequested.type,
+      onSuccess: bugsReceived.type,
+      onError: bugsRequestFailed.type,
+    })
+  );
+};
+
+export const addBug = (bug) =>
+  apiCallBegan({
+    url,
+    method: "post",
+    data: bug,
+    onSuccess: bugAdded.type,
+  });
+
+export const resolveBug = (bugId) =>
+  apiCallBegan({
+    url: `${url}/${bugId}`,
+    method: "patch",
+    data: { resolved: true },
+    onSuccess: bugResolved.type,
+  });
+
+export const assignBugToUser = (bugId, userId) =>
+  apiCallBegan({
+    url: `${url}/${bugId}`,
+    method: "patch",
+    data: { userId },
+    onSuccess: bugAssignedToUser.type,
+  });
+
+// Memoizing selector
+export const getUnresolvedBugs = createSelector(
+  (state) => state.entities.bugs,
+  (state) => state.entities.projects,
+  (bugs, projects) => bugs.filter((bug) => !bug.resolved)
+);
+
+export const getBugsByUser = (userId) =>
+  createSelector(
+    (state) => state.entities.bugs,
+    (bugs) => bugs.filter((bug) => bug.userId === userId)
+  );
