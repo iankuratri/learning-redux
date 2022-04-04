88b6eaa262f27b127551eb487e6aa3bdbe0c7303
Ankur Atri <ankur@zenways.io>
2022-04-04 16:07:20 +0530

    accessing bugs from store in functional component using hooks

--- 10 Integration with React/frontend/src/components/BugsFunction.jsx
+++ 10 Integration with React/frontend/src/components/BugsFunction.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadBugs } from "./../store/bugs";
 
 export default function BugsFunction() {
-  return <div>BugsFunction</div>;
+  const dispatch = useDispatch();
+  const bugs = useSelector((state) => state.entities.bugs.list);
+
+  useEffect(() => {
+    dispatch(loadBugs());
+  });
+
+  return (
+    <div>
+      <h2>Bugs Function Component</h2>
+      <ul>
+        {bugs.map((bug) => (
+          <li key={bug.id}>{bug.description}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
