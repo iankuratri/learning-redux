2392a626baf9abcff60536451b9cfa6fdccc593d
Ankur Atri <ankuratri.in@gmail.com>
2022-04-07 21:41:30 +0530

    redux-logger and redux-devtools-extension added

--- 11 React Redux/src/store/store.js
+++ 11 React Redux/src/store/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// Logger with default options
+import logger from "redux-logger";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
